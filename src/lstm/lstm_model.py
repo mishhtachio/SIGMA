1ea3bbefbb0617b2b2bdc8f1c0289db5635ade35
mishhtachio <mishel.muhammed@gmail.com>
2025-12-25 14:40:35 +0400

    Update lstm_model.py

--- src/lstm/lstm_model.py
+++ src/lstm/lstm_model.py
@@ -1,10 +1,7 @@
 import torch.nn as nn
 
 class LSTMForecast(nn.Module):
-    """
-    Simple LSTM for time-series forecasting
-    """
-
+    #Simple LSTM for time-series forecasting
     def __init__(self, input_size=1, hidden_size=64, num_layers=1):
         super().__init__()
 
@@ -16,19 +13,17 @@ class LSTMForecast(nn.Module):
             batch_first=True
         )
 
-        #fully connected 
+        #Fully connected 
         self.fc = nn.Linear(hidden_size, 1)
 
     def forward(self, x):
-        """
-        x shape: (batch, sequence_length, input_size)
-        """
+        #Required shape = x: (batch, sequence_length, input_size)
         out, _ = self.lstm(x)
 
-        # Take output from last timestep
+        #Take output from last timestep
         out = out[:, -1, :]
 
-        #final prediction
+        #Final prediction
         out = self.fc(out)
 
         return out.squeeze()
